0b1a5800cbf530731489656c0948c12ea253dfb5
RLucer <lucero.ricardo@gmail.com>
2024-06-07 21:50:57 -0400

    add method search by id product

--- src/routes/Product.router.js
+++ src/routes/Product.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getAllProducts } = require('../controllers/Product.controller');
+const { createProduct, getAllProducts, getByIdProducts } = require('../controllers/Product.controller');
 const auth = require('../middlewares/auth');
 
 router.post('/', auth, createProduct);
 router.get('/',  getAllProducts);
+router.get('/id',  getByIdProducts);
+
 
 module.exports = router
