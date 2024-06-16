48ccee47e02e4136700c4cd22800107c9bdbfa8a
RLucer <lucero.ricardo@gmail.com>
2024-06-16 04:20:23 -0400

    mp

--- src/controllers/Mercadopago.controller.js
+++ src/controllers/Mercadopago.controller.js
@@ -8,14 +8,14 @@ const { MercadoPagoConfig, Preference } = require('mercadopago');
 
 // // Configura tus credenciales de acceso
 const mp = new MercadoPagoConfig({
-access_token: 'TEST-8656165071536227-061600-b0de2d2f618d0f3ba6f1eab7744b1b49-1048615732', // Reemplaza con tu access token
+access_token: 'TEST-1899159482044925-052500-1b30f6160798927355decf7da80b604b-56139632', // Reemplaza con tu access token
 });
 
 const payProduct = async (req, res) => {  
     
     try {
         mercadopago.configure({
-            access_token: 'TEST-8656165071536227-061600-b0de2d2f618d0f3ba6f1eab7744b1b49-1048615732'//  process.env.ACCESS_TOKEN
+            access_token: 'TEST-1899159482044925-052500-1b30f6160798927355decf7da80b604b-56139632'//  process.env.ACCESS_TOKEN
         })
 
         const preference = req.body
