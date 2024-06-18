d36c2f42a5730f4f2f9ba59d938713eee9f93c01
RLucer <lucero.ricardo@gmail.com>
2024-06-18 16:04:44 -0400

    server api pro5

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,7 @@ const mercadopagoRoutes = require('./Mercadopago.router');
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
-router.use('/mercadopago', mercadopagoRoutes);
-
+router.use('/mercadopago', mercadopagoRoutes);ß
 module.exports = router;
 //http:localhost:3000/v1/users
 //http:localhost:3000/v1/products
